35b2e0267cea10f0eca744d67ae04050825c4bda
aarav6427 <78213969+aarav6427@users.noreply.github.com>
2024-11-09 01:52:33 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
 api_id = 2645474 #change_with_your_Api_id.
 api_hash = "6c9a5044d2f2c2350ac20b3838a7896e" #change_with_your_Api_hash.
-bot_token = "7595907385:AAFxOWCPWCa57YrHyS0THz6TbiNRZdY-GK4" #change_with_your_bot_token.
+bot_token = "7919367706:AAHHUP6WcmYh6INvMFqEjL_k9EWQcyYkKK4" #change_with_your_bot_token.
 auth_users = [929216155] #change_with_your_user_id.
 sudo_user = 929216155 #change_with_your_user_id.
 log_channel = -2058900204 #change_with_your_channel_id.
